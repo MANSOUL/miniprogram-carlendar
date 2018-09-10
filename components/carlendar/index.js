656b82eef19bd4d5021fcf781b069dfc74ca6cb1
KuangGuanghu <1073345410@qq.com>
2018-09-10 18:05:24 +0800

    fix: 日历数据渲染抖动

--- components/carlendar/index.js
+++ components/carlendar/index.js
@@ -59,7 +59,7 @@ Component({
     },
     handleTouchStart(e) {
       let touch = e.touches[0]
-      this.prevPageX = touch.pageX
+      this.prevPageX = this.touchStartX = touch.pageX
     },
     handleTouchMove(e) {
       let touch = e.changedTouches[0]
@@ -79,6 +79,9 @@ Component({
       })
     },
     handleTouchEnd(e) {
+      if (Math.abs(e.changedTouches[0].pageX - this.touchStartX) < 15) {
+        return
+      }
       if (this.dir === 'prev') {
         this.translate = 0
       }
@@ -97,13 +100,34 @@ Component({
       else if(this.translate === 0) { // next
         offIndex = -1
       }
-      this.setData({
-        translaterStyle: `transform: translateX(-100%); transition-duration: 0ms;`
-      })
+      // this.setData({
+      //   translaterStyle: `transform: translateX(-100%); transition-duration: 0ms;`
+      // })
       this.translate = -100
       this.setDateDatas(offIndex)
     },
     setDateDatas(offset, firstEnter) {
+      if (firstEnter) {
+        this.fillData(offset)
+      }
+      else {
+        // 为了防抖
+        let currentDates = null
+        if (offset === 1) {
+          currentDates = this.data.nextDates
+        }
+        else {
+          currentDates = this.data.prevDates
+        }
+        this.setData({
+          currentDates
+        }, () => {
+          this.fillData(offset, `transform: translateX(-100%); transition-duration: 0ms;`)
+        })
+      }
+    },
+
+    fillData(offset, translaterStyle = null) {
       let dayObj = carlendar.getDays(offset)
       let year = carlendar.getYear()
       let month = carlendar.getMonth()
@@ -120,7 +144,8 @@ Component({
         // 月份改变，默认选中第一天
         selectedDate: (month === originMonth && year === originYear ? today : 1),
         originYear: originYear,
-        originMonth: originMonth
+        originMonth: originMonth,
+        translaterStyle
       }, () => {
         // triggle month change
         let year = this.data.year
@@ -139,7 +164,9 @@ Component({
   created() {
     this.dir = ''
     this.prevPageX = 0
+    this.touchStartX = 0
     this.translate = -100
+    this.isTap = false
   },
 
   attached() {
