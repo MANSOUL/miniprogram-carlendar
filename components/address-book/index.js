c5024d6640735c057c8079f28bb955d7b46543d7
MANSOUL <xiaohu12347@gmail.com>
2019-05-17 19:10:39 +0800

    fix: index tip

--- components/address-book/index.js
+++ components/address-book/index.js
@@ -1,3 +1,7 @@
+let charBarHeight = 0;
+let charBarOffTop = 0;
+let tagTopArr = [];
+
 const data = {};
 for (let index = 97; index < 123; index++) {
   const c = String.fromCharCode(index);
@@ -17,6 +21,56 @@ Component({
     chars: {
       type: Array,
       value: Object.keys(data)
+    }
+  },
+  data: {
+    scrollTop: 0,
+    currentIndex: -1
+  },
+  ready() {
+    this.getBoundingRect('#char__bar', res => {
+      charBarOffTop = res.top;
+      charBarHeight = res.height;
+    });
+    this.data.chars.forEach(c => {
+      this.getBoundingRect(`#tag-${c}`, res => {
+        tagTopArr.push(res.top);
+      });
+    });
+  },
+  methods: {
+    getBoundingRect(id, fn) {
+      const q = this.createSelectorQuery();
+      const $bar = q.select(id);
+      $bar.boundingClientRect(fn);
+      q.exec();
+    },
+    handleTouchStart(e) {
+      this.move(e.touches[0]);
+    },
+    handleTouchMove(e) {
+      this.move(e.changedTouches[0]);
+    },
+    handleTouchEnd(e) {
+      // this.move(e.changedTouches[0]);
+      this.setData({
+        currentIndex: -1
+      });
+    },
+    move(t) {
+      const len = this.data.chars.length;
+      const clientY = t.clientY;
+      const rY = clientY - charBarOffTop;
+      let index = Math.floor(rY / (charBarHeight / len));
+      index = index < 0 ? 0 : index;
+      index = index >= len ? len - 1 : index;
+      this.scroll(index);
+    },
+    scroll(index) {
+      this.setData({
+        scrollTop: tagTopArr[index],
+        currentIndex: index
+      });
     }
   }
 });
