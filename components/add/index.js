2f50816a75b6c3501b3577d51388997cae351edc
MANSOUL <xiaohu12347@gmail.com>
2019-05-17 19:39:08 +0800

    fix: data from page

--- components/add/index.js
+++ components/add/index.js
@@ -1,3 +1,16 @@
-Component({
+const data = {};
+for (let index = 97; index < 123; index++) {
+  const c = String.fromCharCode(index);
+  const arr = [];
+  for (let i = 0; i < (2 + Math.floor(Math.random() * 8)); i++) {
+    arr.push(`${c}${Math.floor(Math.random() * 1000)}`);
+  }
+  data[c] = arr;
+}
 
+Component({
+  data: {
+    dict: data,
+    chars: Object.keys(data)
+  }
 });
--- components/address-book/index.js
+++ components/address-book/index.js
@@ -2,25 +2,15 @@ let charBarHeight = 0;
 let charBarOffTop = 0;
 let tagTopArr = [];
 
-const data = {};
-for (let index = 97; index < 123; index++) {
-  const c = String.fromCharCode(index);
-  const arr = [];
-  for (let i = 0; i < (2 + Math.floor(Math.random() * 8)); i++) {
-    arr.push(`${c}${Math.floor(Math.random() * 1000)}`);
-  }
-  data[c] = arr;
-}
-
 Component({
   properties: {
     data: {
       type: Object,
-      value: data
+      value: {}
     },
     chars: {
       type: Array,
-      value: Object.keys(data)
+      value: []
     }
   },
   data: {
